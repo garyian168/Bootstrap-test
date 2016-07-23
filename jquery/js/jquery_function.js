b577e5fa1fef974610ff6fd59babd6997cd9d5e4
kevin <271880517@qq.com>
2016-07-23 10:33:57 +0800

    push the file

--- jquery/js/jquery_function.js
+++ jquery/js/jquery_function.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
-	$("#show").click(function(){
-		$("p").show();
+	$("#button1").click(function(){
+		$("#pp1").fadeIn(3000);
 	});
 
-	$("#hide").click(function(){
-		$("p").hide();
-	})
+
+	$("#button2").click(function(){
+		$("#pp2").fadeOut(1000);
+	});
+
+	$("#button3").click(function(){
+		$("#pp3").toggle(1000);
+	});
+
+	// $("#button4").click(function(){
+	// 	$("#pp4").fadeTo("fast",0.3);
+	// });
+
+	$("#button4").click(function(){
+		$("#pp4").slideUp();
+	});
+
+
+	$("#button5").click(function(){
+		$("#pp5").animate({right: '200px'});
+	});
+
+	$("#button6").click(function(){
+		$("#pp6").animate({
+			left: '250px',
+			opacity:'0.5',
+			height:'150px',
+			width:'150px'
+		});
+	});
+
+
 });
